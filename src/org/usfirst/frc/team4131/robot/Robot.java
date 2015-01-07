956876be23c1fd29b5528f4b42493308782d6261
endreman0 <endreman@squidgey.com>
2015-01-06 23:16:39 -0800

    Read everything from encoders and sonar to SmartDashboard
    Sonar is as of yet not calibrated in any way, shape or form; unless 5cm and 75cm are roughly the same.

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4131.robot;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -11,21 +12,25 @@ public class Robot extends SampleRobot{
 	private RobotDrive drive = new RobotDrive(0, 1);
 	private Joystick cont1 = new Joystick(0);//Game controllers
 	private Encoder encLeft = new Encoder(0, 1), encRight = new Encoder(2, 3);
+	private AnalogInput sonar = new AnalogInput(0);
 	public Robot(){}
 	public void autonomous(){
 		
 	}
 	public void operatorControl(){
 		while(isOperatorControl() && isEnabled()){
-			drive.arcadeDrive((1-cont1.getRawAxis(3)) * cont1.getRawAxis(1), -cont1.getRawAxis(4));//Y and X axes for right thumbstick
+			drive.arcadeDrive((1-cont1.getRawAxis(3)) * cont1.getRawAxis(1), -cont1.getRawAxis(4));//Y axis of left stick, X of right
 			SmartDashboard.putString("Left Encoder", encLeft.getStopped() ? "Stopped" : !encLeft.getDirection() ? "Forward" : "Backward");
 			SmartDashboard.putString("Right Encoder", encRight.getStopped() ? "Stopped" : encRight.getDirection() ? "Forward" : "Backward");
+			SmartDashboard.putNumber("Left Encoder Value", -encLeft.getDistance());
+			SmartDashboard.putNumber("Right Encoder Value", encRight.getDistance());
+			SmartDashboard.putNumber("Sonar (cm)", sonar.getVoltage());//0.009766
 			Timer.delay(0.005);
 		}
 	}
 	public void test(){
 		while(isTest() && isEnabled()){
-			
+			drive.arcadeDrive((1-cont1.getRawAxis(3)) * cont1.getRawAxis(1), -cont1.getRawAxis(4));
 			Timer.delay(0.005);
 		}
 	}
