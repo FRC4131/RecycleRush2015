2064208a88b1b5f542e2f3915cce95166bbec593
endreman0 <endreman@squidgey.com>
2015-01-19 13:28:13 -0800

    Refine gyro centering

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -11,15 +11,13 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot{
 	private RobotDrive drive = new RobotDrive(0, 1);
-	private Joystick cont = new Joystick(0);
-	private JoystickButton buttonReset = new JoystickButton(cont, 2);//B
+	private Joystick controller = new Joystick(0);
 	private Encoder encLeft = new Encoder(0, 1), encRight = new Encoder(2, 3);
 	private AnalogInput sonar = new AnalogInput(0);
 	private final double SONAR_MULT = sonar.getLSBWeight()*Math.exp(-9);
@@ -49,9 +47,13 @@ public class Robot extends SampleRobot{
 					SmartDashboard.putNumber("Accel 2 Y", accel2.getY());
 					SmartDashboard.putNumber("Accel 2 Z", accel2.getZ());
 					SmartDashboard.putNumber("Gyro", gyro.getAngle());
-					SmartDashboard.putNumber("Gyro speed", -gyro.getAngle()/200);
 					SmartDashboard.putNumber("Temperature (C)", getTemp(true));
-					SmartDashboard.putNumber("Temperature (F)", getTemp(false ));
+					SmartDashboard.putNumber("Temperature (F)", getTemp(false));
+					if(controller.getRawButton(controller.getAxisCount()==6 ? 2 : 3)){//B button on either controller
+						encLeft.reset();
+						encRight.reset();
+						gyro.reset();
+					}
 					Timer.delay(0.005);
 				}
 			}
@@ -63,21 +65,23 @@ public class Robot extends SampleRobot{
 			else drive.drive(0, 0);
 			SmartDashboard.putNumber("Sonar (in)", getSonar(true));
 			SmartDashboard.putNumber("Sonar (cm)", getSonar(false));*/
-			drive.arcadeDrive(0, -gyro.getAngle()/100);
+			
+			double angle = -gyro.getAngle() % 360;
+			if(Math.abs(angle)>180) angle = Math.copySign(Math.abs(angle)-180, -angle);//-270 becomes 90, 315 becomes -45
+			if(Math.abs(angle)<1){
+				drive.arcadeDrive(0, 0, false);//Stop
+			}else if(Math.abs(angle)<15){
+				drive.arcadeDrive(0, Math.copySign(0.1, angle), false);//Move slowly
+			}else{
+				drive.arcadeDrive(0, Math.copySign(0.3, angle), false);//Move quickly
+			}
 			Timer.delay(0.005);
 		}
 	}
 	public void operatorControl(){
-		boolean driveType = cont.getAxisCount()==6;//True means Xbox One controller, with variable-input triggers on axes 2 and 3.
-													//False means Logitech controller with buttons instead of triggers.
 		while(isOperatorControl() && isEnabled()){
-			if(driveType) drive.arcadeDrive(cont.getRawAxis(1), cont.getRawAxis(4));//Y axis of left stick, X of right
-			else drive.arcadeDrive(cont.getRawAxis(1), cont.getRawAxis(2));
-			if(buttonReset.get()){
-				encLeft.reset();
-				encRight.reset();
-				gyro.reset();
-			}
+			if(controller.getAxisCount()==6) drive.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(4));//Y axis of left stick, X of right
+			else drive.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(2));
 			Timer.delay(0.005);
 		}
 	}
