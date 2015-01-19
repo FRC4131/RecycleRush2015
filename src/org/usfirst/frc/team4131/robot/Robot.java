04383acc68cef8d952107895093f0b9731ba3025
endreman0 <endreman@squidgey.com>
2015-01-19 13:45:06 -0800

    Transfer previously autonomous tasks to buttons

--- src/org/usfirst/frc/team4131/robot/Robot.java
+++ src/org/usfirst/frc/team4131/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -18,6 +19,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends SampleRobot{
 	private RobotDrive drive = new RobotDrive(0, 1);
 	private Joystick controller = new Joystick(0);
+	private JoystickButton buttonReset = new JoystickButton(controller, 2), buttonCenter = new JoystickButton(controller, 3),
+			buttonApproach = new JoystickButton(controller, 4);//B, X and Y
 	private Encoder encLeft = new Encoder(0, 1), encRight = new Encoder(2, 3);
 	private AnalogInput sonar = new AnalogInput(0);
 	private final double SONAR_MULT = sonar.getLSBWeight()*Math.exp(-9);
@@ -49,7 +52,7 @@ public class Robot extends SampleRobot{
 					SmartDashboard.putNumber("Gyro", gyro.getAngle());
 					SmartDashboard.putNumber("Temperature (C)", getTemp(true));
 					SmartDashboard.putNumber("Temperature (F)", getTemp(false));
-					if(controller.getRawButton(controller.getAxisCount()==6 ? 2 : 3)){//B button on either controller
+					if(buttonReset.get()){
 						encLeft.reset();
 						encRight.reset();
 						gyro.reset();
@@ -61,27 +64,26 @@ public class Robot extends SampleRobot{
 	}
 	public void autonomous(){
 		while(isAutonomous() && isEnabled()){
-			/*if(getSonar(true)>24) drive.drive(0.1, 0);
-			else drive.drive(0, 0);
-			SmartDashboard.putNumber("Sonar (in)", getSonar(true));
-			SmartDashboard.putNumber("Sonar (cm)", getSonar(false));*/
-			
-			double angle = -gyro.getAngle() % 360;
-			if(Math.abs(angle)>180) angle = Math.copySign(Math.abs(angle)-180, -angle);//-270 becomes 90, 315 becomes -45
-			if(Math.abs(angle)<1){
-				drive.arcadeDrive(0, 0, false);//Stop
-			}else if(Math.abs(angle)<15){
-				drive.arcadeDrive(0, Math.copySign(0.1, angle), false);//Move slowly
-			}else{
-				drive.arcadeDrive(0, Math.copySign(0.3, angle), false);//Move quickly
-			}
 			Timer.delay(0.005);
 		}
 	}
 	public void operatorControl(){
 		while(isOperatorControl() && isEnabled()){
-			if(controller.getAxisCount()==6) drive.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(4));//Y axis of left stick, X of right
-			else drive.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(2));
+			if(buttonCenter.get()){
+				double angle = -gyro.getAngle() % 360;
+				if(Math.abs(angle)>180) angle = Math.copySign(Math.abs(angle)-180, -angle);//-270 becomes 90, 315 becomes -45
+				if(Math.abs(angle)<1){
+					drive.arcadeDrive(0, 0, false);//Stop
+				}else if(Math.abs(angle)<15){
+					drive.arcadeDrive(0, Math.copySign(0.1, angle), false);//Move slowly
+				}else{
+					drive.arcadeDrive(0, Math.copySign(0.3, angle), false);//Move quickly
+				}
+			}else if(buttonApproach.get()){
+				drive.drive(getSonar(true)>24 ? -0.2 : 0, 0);
+			}else{
+				drive.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(4));//Y axis of left stick, X of right
+			}
 			Timer.delay(0.005);
 		}
 	}
